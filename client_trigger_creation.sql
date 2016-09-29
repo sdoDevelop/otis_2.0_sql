33211e9d9b82b61f9c70adad995d8dfd6163bdcd
Sean O'Connell <sdoconnell92@gmail.com>
2016-09-28 22:15:35 -0500

    Added files

---
+++ client_trigger_creation.sql
@@ -0,0 +1,9 @@
+-- ##################################
+-- ###### Create Otis Database ######
+-- ##################################
+
+
+
+-- Triggers
+	Create Trigger events_update_trigger 
+		After Update On events_
